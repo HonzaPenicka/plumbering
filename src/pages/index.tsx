160d0cbd9d1399100ff8ab6c02f6bd8bb4cbb799
Honza Pěnička <97037180+HonzaPenicka@users.noreply.github.com>
2025-03-10 21:09:45 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,64 +60,64 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Plumber Prague - Profesionální instalatéři v Praze</title>
+        <title>YOUR BOB - Profesionální instalatéři v Praze</title>
         <meta
           name="description"
-          content="Plumber Prague nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
+          content="Your Bob nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
         />
         <meta
           name="keywords"
-          content="instalatéři Praha, plumber Prague, vodoinstalace, oprava potrubí, montáž bezpečnostních zámků, výměna žárovek"
+          content="instalatéři Praha, Your Bob, vodoinstalace, oprava potrubí, montáž bezpečnostních zámků, výměna žárovek"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Open Graph Meta Tags */}
         <meta
           property="og:title"
-          content="Plumber Prague - Profesionální instalatéři v Praze"
+          content="Your Bob - Profesionální instalatéři v Praze"
         />
         <meta
           property="og:description"
-          content="Plumber Prague nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
+          content="Your Bob nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
         />
-        <meta property="og:image" content="/img/hero/desktop.webp" />
-        <meta property="og:url" content="https://www.plumberprague.cz" />
+        <meta property="og:image" content="/img/hero/yourbob.jpeg" />
+        <meta property="og:url" content="https://www.yourbob.cz" />
         <meta property="og:type" content="website" />
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
-          content="Plumber Prague - Profesionální instalatéři v Praze"
+          content="Your Bob - Profesionální instalatéři v Praze"
         />
         <meta
           name="twitter:description"
-          content="Plumber Prague nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
+          content="Your Bob nabízí ověřené instalatéry v Praze. Kvalitní služby, dostupnost během hodiny, bezplatný odhad ceny."
         />
-        <meta name="twitter:image" content="/img/hero/desktop.webp" />
+        <meta name="twitter:image" content="/img/hero/yourbob.jpeg" />
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         {/* Security */}
         <meta name="referrer" content="no-referrer-when-downgrade" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://www.www.plumberprague.cz" />
+        <link rel="canonical" href="https://www.yourbob.cz" />
       </Head>
       <div
         className={`${geistSans.variable} ${geistMono.variable} grid container mx-auto items-center pb-4 font-[family-name:var(--font-geist-sans)]`}
       >
-        <header className="flex justify-between bg-white py-4 px-4 items-center text-black font-bold text-3xl border-b-2 border-b-[#FFCC33]">
+        <header className="flex justify-between bg-white py-4 px-4 items-center text-black font-bold text-3xl border-b-2 border-b-blue-500">
           <a href="/">
-            <h1>PLUMBER PRAGUE</h1>
+            <h1>YOUR BOB</h1>
           </a>
           <button onClick={handleIsMenuOpen}>
             <GiHamburgerMenu className="flex justify-start items-start" />
           </button>
         </header>
         {isMenuOpen && (
-          <nav className="p-4 absolute bg-white top-14 z-10 font-semibold text-xl container border-2 border-[#FFCC33] uppercase mx-auto">
+          <nav className="p-4 absolute bg-white top-14 z-10 font-semibold text-xl container border-2 border-blue-500 uppercase mx-auto">
             <ul className="flex flex-col gap-4 text-right">
               <li>
                 <a
                   href="/"
-                  className="hover:underline-offset-2 hover:underline"
+                  className="hover:underline-offset-2 hover:underline hover:decoration-red-500"
                 >
                   Home
                 </a>
@@ -125,9 +125,17 @@ export default function Home() {
               <li>
                 <a
                   href="/price-list"
-                  className="hover:underline-offset-2 hover:underline"
+                  className="hover:underline-offset-2 hover:underline hover:decoration-red-500"
                 >
                   Price List
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about"
+                  className="hover:underline-offset-2 hover:underline hover:decoration-red-500"
+                >
+                  About
                 </a>
               </li>
             </ul>
@@ -136,47 +144,20 @@ export default function Home() {
         <main className="grid gap-4 lg:gap-8 items-left lg:items-center">
           <div>
             <section
-              className="px-4 py-4 lg:py-8 bg-[url('/img/hero/mobile.png')] md:bg-[url('/img/hero/desktop.png')] bg-cover md:bg-contain bg-bottom lg:bg-center"
+              className="px-4 py-4 lg:py-24 bg-[url('/img/hero/yourbob.jpeg')] md:bg-[url('/img/hero/yourbob.jpeg')] bg-cover bg-bottom md:bg-top"
               style={{
                 backgroundRepeat: 'no-repeat',
               }}
               aria-label="Hero Section"
             >
-              <h1 className="justify-center flex text-2xl md:text-4xl lg:text-6xl font-bold text-black pt-4 pb-8 lg:pt-0 lg:pb-16 uppercase">
-                Verified Prague plumber
+              <h1 className="justify-center flex text-3xl md:text-4xl lg:text-6xl font-bold pt-4 pb-8 lg:pt-0 lg:pb-16 uppercase text-white">
+                Verified Your Bob
               </h1>
-
-              <div className="text-xl grid gap-8 lg:gap-16">
-                <div className="flex gap-4 items-center">
-                  <BulpIcon />
-
-                  <div className="text-black font-semibold lg:text-3xl backdrop-blur-sm rounded-3xl">
-                    Qualified plumbers
-                  </div>
-                </div>
-
-                <div className="flex gap-4 items-center">
-                  <BulpIcon />
-
-                  <div className="text-black font-semibold lg:text-3xl backdrop-blur-sm rounded-3xl">
-                    Availability within 1 hour <br />
-                    from booking
-                  </div>
-                </div>
-
-                <div className="flex gap-4 items-center">
-                  <BulpIcon />
-
-                  <div className="text-black font-semibold lg:text-3xl backdrop-blur-sm rounded-3xl">
-                    Free price estimate
-                  </div>
-                </div>
-              </div>
               <div className="flex items-center justify-center py-24">
                 <button
                   onClick={scrollToForm}
-                  className={`backdrop-blur-sm text-yellow-400 uppercase font-bold text-2xl lg:text-4xl py-4 px-8 lg:py-8 lg:px-32 rounded-xl ring-2 ring-yellow-400 hover:ring-yellow-400 ${
-                    isClicked ? 'ring-[#33CC99]' : ''
+                  className={`backdrop-blur-sm text-white uppercase font-bold text-2xl lg:text-4xl py-4 px-8 lg:py-8 lg:px-32 rounded-xl ring-2 ring-white hover:ring-red-500 hover:text-red-500 ${
+                    isClicked ? 'ring-blue-500' : ''
                   } transition duration-300`}
                   aria-label="Contact us"
                 >
@@ -230,7 +211,7 @@ export default function Home() {
                 </div>
               </div>
             </section>
-            <section className="bg-[#FFCC33] md:bg-white md:border-[#FFCC33] md:border-4">
+            <section className="bg-blue-100 md:bg-white md:border-blue-500 md:border-2">
               <h2 className="justify-center flex text-2xl font-bold pb-4 pt-8 uppercase">
                 Verified Prague plumber
               </h2>
@@ -383,9 +364,9 @@ export default function Home() {
               </div>
             </>
           </section>
-          <section className="bg-[#FFCC33] md:bg-white md:border-[#FFCC33] md:border-4 text-pretty">
+          <section className="bg-blue-100 md:bg-white md:border-blue-500 md:border-2 text-pretty">
             <h2 className="justify-center flex text-2xl font-bold py-8 uppercase">
-              Why choose Plumber Prague
+              Why choose YOUR BOB
             </h2>
             <div className="px-8 pb-8 grid gap-4">
               <div className="flex flex-col gap-4 items-center">
@@ -445,10 +426,10 @@ export default function Home() {
                 Click to text your problem on WhatsApp!
               </h2>
               <a
-                href="https://chat.whatsapp.com/CjPS5uDuFGp5IeX4gHhKZK"
+                href="https://wa.me/message/O2XFDY6ZCZHYD1"
                 className="flex flex-col items-center justify-center gap-8"
               >
-                <button className="uppercase text-2xl font-bold border-2 border-[#33CC99] rounded-lg p-4 text-[#33CC99] hover:text-[#FFCC33]">
+                <button className="uppercase text-2xl font-bold border-2 border-blue-500 rounded-lg p-4 text-blue-500 hover:text-red-500 hover:border-red-500">
                   Contact us!
                 </button>
 
@@ -457,9 +438,9 @@ export default function Home() {
             </div>
           </section>
         </main>
-        <footer className="px-4 grid md:block gap-2 border-t-2 border-t-[#FFCC33]">
+        <footer className="px-4 grid md:block gap-2 border-t-2 border-blue-500">
           <div className="bg-white py-4 items-center text-black font-bold text-3xl">
-            <div>PLUMBER PRAGUE</div>
+            <div>YOUR BOB</div>
           </div>
           <div className="grid gap-4">
             <div className="flex flex-col md:flex-row gap-2 md:justify-between">
@@ -471,8 +452,9 @@ export default function Home() {
                 aria-label="Email us"
               >
                 <MdMailOutline height={16} width={16} />
-                plumberprague@gmail.com
+                Info@yourbob.cz
               </a>
+              <p className=' hover:underline hover:underline-offset-4 md:text-xl lg:text-2xl'>IČ: 04383257</p>
               <a
                 className="flex items-center gap-2 hover:underline hover:underline-offset-4 md:text-xl lg:text-2xl"
                 href=""
@@ -480,11 +462,11 @@ export default function Home() {
                 rel="noopener noreferrer"
                 aria-label="Call us"
               >
-                <FaPhone height={16} width={16} />+ 420 123 456 789
+                <FaPhone height={16} width={16} />+420 704 901 902
               </a>
             </div>
             <div className="text-xs text-center py-4">
-              &copy; 2024 Plumber Prague s.r.o. — All rights reserved.
+              &copy; 2024 Your Bob s.r.o. — All rights reserved.
             </div>
           </div>
         </footer>
